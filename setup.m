17a355c86d772cd95df8264df94523dc377ceda6
zijunwei <zijwei@cs.stonybrook.edu>
2014-12-26 15:51:33 -0500

    setup file modified

--- setup.m
+++ setup.m
@@ -1,9 +1,14 @@
-clear;
-clc;
-close all;
+function setup(id)
+if id==1
+loc='zjlocal';
+elseif id==2
+loc='zjserver';
+end
 
+fprintf	('This is runing on %s \n',loc);
 
 addpath_recurse('./voc-release5');
 addpath_recurse('./MyGradFuncs');
 run('./vlfeat/toolbox/vl_setup.m');
 
+end
